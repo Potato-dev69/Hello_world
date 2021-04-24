3d5065e6b498e0ecb2fcae6fb6f5d2208656155e
Potato-dev69 <annemijn@ogilvie.gq>
2021-04-24 18:32:06 +0000

    Pincode steler

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -2,5 +2,21 @@
 
 int main()
 {
-    std::cout << "Hello world!\n";
+    std::cout << "Hallo mensen\n";
+    std::cout << "Potato\n";
+    int potato = 69;
+    std::cout << potato << "\n";
+    potato = 5;  
+    std::cout << potato << "\n";
+    std::cout << "Hallo, vul hier je pincode in asjeblieft";
+    std::cin >> potato; 
+    std::cout << potato << "\n";
+    if (potato == 9669) 
+    {
+        std::cout << "You've found the nice, well done human being!" << std::endl; 
+    }
+    else 
+    {
+        std::cout << "Thanks for the money human being!\n";
+    }
 }
